5ab720a4b9b91b439705e1bbcde1a40cd7628248
0x2f0713 <namhaiha0308@gmail.com>
2021-02-11 23:37:12 +0700

    final of final of final commit in 2020

--- EnrollUdemy.py
+++ EnrollUdemy.py
@@ -20,14 +20,18 @@ def getInfinityFreeCookie():
     getRawData_request = requests.post("https://mess.0x2f0713.cf/api/enroll_udemy/logs",verify=False)
     print(getRawData_request.text)
     x = re.findall("[a-f0-9]{32}", getRawData_request.text)
-    for index_x,i in enumerate(x):
-        x[index_x] = codecs.decode(i,"hex")
-    iv = x[1]
-    ct = x[2]
-    cipher = AES.new(x[0], AES.MODE_CBC, iv)
-    res = cipher.decrypt(ct).hex()
-    print(res)
-    return res
+    if x:
+        for index_x,i in enumerate(x):
+            x[index_x] = codecs.decode(i,"hex")
+        iv = x[1]
+        ct = x[2]
+        cipher = AES.new(x[0], AES.MODE_CBC, iv)
+        res = cipher.decrypt(ct).hex()
+        print(res)
+        return res
+    else:
+        print("No need cookie")
+        return "b9cb98eab7325b9bac3a43cda928a72b"
 
 
 class EnrollThreading(threading.Thread):
