e73107441b2bea3c8af225acf388b972a23ac4f3
0x2f0713 <namhaiha0308@gmail.com>
2021-08-18 23:18:50 +0700

    fix enroll enrolled course

--- get-enrolled.py
+++ get-enrolled.py
@@ -3,10 +3,10 @@ import requests
 Session = Udemy('namhaiha0308@gmail.com','qweqwerrr123456789')
 newfile = open("Enrolled.txt","a+")
 # amount = Session.getEnrolledCourseAmount() // 100 + 2
-amount = 10
+amount = 2
 for i in range(1,amount):
     data = Session.getEnrolledCoursesPerPage("https://www.udemy.com/api-2.0/users/me/subscribed-courses?page_size=100&page={page}&fields=@min".format(page = i))
     print(i)
     for y in data:
-        newfile.write(y['published_title']+"\n")
+        newfile.write("https://www.udemy.com/course/"+y['published_title']+"\n")
         #print(y['published_title'])
