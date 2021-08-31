828c41689a06d32742c465f338476f9ee3460eee
0x2f0713 <namhaiha0308@gmail.com>
2021-08-31 22:05:19 +0700

    fix cookies

--- get-enrolled.py
+++ get-enrolled.py
@@ -1,12 +1,13 @@
 from Udemy import Udemy
 import requests
 Session = Udemy('namhaiha0308@gmail.com','qweqwerrr123456789')
-newfile = open("Enrolled.txt","a+")
-# amount = Session.getEnrolledCourseAmount() // 100 + 2
-amount = 2
-for i in range(1,amount):
-    data = Session.getEnrolledCoursesPerPage("https://www.udemy.com/api-2.0/users/me/subscribed-courses?page_size=100&page={page}&fields=@min".format(page = i))
+newfile = open("Enrolled.txt","w")
+amount = Session.getEnrolledCourseAmount() // 100 + 2
+start = 4
+end = 3
+for i in range(start,start + 1):
+    data = Session.getEnrolledCoursesPerPage("https://www.udemy.com/api-2.0/users/me/subscribed-courses?ordering=+enroll_time&page_size=100&page={page}&fields=@min".format(page = i))
     print(i)
     for y in data:
-        newfile.write("https://www.udemy.com/course/"+y['published_title']+"\n")
+        newfile.write(y['published_title']+"\n")
         #print(y['published_title'])
